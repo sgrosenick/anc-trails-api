4e93f9fb86fa4bdc43d8aa75a0682af9dc8d0827
Samuel Grosenick <srgrosenick@gmail.com>
2020-10-01 17:40:13 -0800

    updated knexfile to include production

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,7 @@ module.exports = {
         conn.run("PRAGMA foreign_keys = ON", done);
       }
     }
-  }
+  },
 
   // staging: {
   //   client: 'postgresql',
@@ -31,20 +31,17 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    }
+  }
 
 };
