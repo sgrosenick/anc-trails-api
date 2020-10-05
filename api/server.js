089fc314b233ebcfa6c30f685919ac59cad8cc93
Samuel Grosenick <srgrosenick@gmail.com>
2020-10-04 21:38:32 -0800

    add cors

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const lessonsRouter = require('../routes/lessons-routes');
 const messagesRouter = require('../routes/messages-routes');
 const streetsRouter = require('../routes/streets-routes');
 
 const server = express();
+server.use(cors());
 server.use(express.json());
 
 server.get('/', (req, res) => {
